67f7f35d9b090418f5fada57612cd73f43ead9f6
christopherbw1994 <christopherbillwiberg@gmail.com>
2022-05-29 16:39:55 +0200

    chris

--- js/jobdropdown.js
+++ js/jobdropdown.js
@@ -12,3 +12,20 @@ let opretArray = new Array("Jeg hørte om jobbet igennem:", "Netværk", "Sociale
     for (let i = 0; i < opretArray.length; ++i) {
         dropdown[dropdown.length] = new Option(opretArray[i], opretArray[i]);
     }
+
+    function myFunction() {
+            console.log("Har trykket");
+            let response = ["Udfyld felt", "Din besked er sendt!"];
+            let empt = document.form1.Titel.value;
+            if (empt === "")
+            {
+            alert( response[0] );
+            return false;
+            }
+            else
+            {
+            alert( response[1] );
+            return true;
+            document.getElementById("demo").innerHTML = "Good day";
+            }
+            }
